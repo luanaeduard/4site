30c126b73e1f13356e1514510e1986d298f57144
luanaeduard <160140564+luanaeduard@users.noreply.github.com>
2024-10-17 11:30:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    '1',
+    'Qual é o país mais populoso do mundo?',
+    'china'
+)
+
+criaCartao(
+    '2',
+    'Qual país tem a maior área territorial?',
+    'Rússia.'
+)
+
+criaCartao(
+    '3',
+    'Em que continente está a Austrália?',
+    'Oceania.'
+)
+
+criaCartao(
+    '4',
+    'Quais são os três países que fazem parte do Benelux?',
+    'Bélgica, Países Baixos e Luxemburgo.'
+)
+
+criaCartao(
+    '5',
+    'Qual é a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    '6',
+    'Que país é conhecido como a "Terra do Sol Nascente"?',
+    'Japão'
+)
+
+criaCartao(
+    '7',
+    'Quais países fazem fronteira com o Brasil?    ',
+    'Argentina, Uruguai, Paraguai, Peru, Colômbia e Venezuela.'
+)
+
+criaCartao(
+    '8',
+    'Onde fica o país mais pequeno do mundo?',
+    'Cidade do Vaticano.'
+)
+
+criaCartao(
+    '9',
+    'Qual é a capital do Japão?',
+    'Tóquio'
+)
